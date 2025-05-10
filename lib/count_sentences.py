beff9e8d2378884c34607aa4f08dc6ea4147b8e6
Frankincense <frankincensewelsey@gmail.com>
2025-05-11 00:13:40 +0300

    Sentence counting

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,47 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__(self, value=""):
+    self.value = value
+
+  @property
+  def value(self):
+    return self._value
+  
+  @value.setter
+  def value(self, value):
+    if isinstance(value, str):
+      self._value = value
+    else:
+      print("The value must be a string.")
+
+  def is_sentence(self):
+    if self.value.endswith("."):
+      return True
+    else:
+      return False
+  
+  def is_question(self):
+    if self.value.endswith("?"):
+      return True
+    else:
+      return False
+
+  def is_exclamation(self):
+    if self.value.endswith("!"):
+      return True
+    else:
+      return False
+  
+  def count_sentences(self):
+    sentence = self.value
+    if sentence == "":
+      return 0
+    for char in ["?", "!"]:
+      sentence = sentence.replace(char, ".")
+    print(sentence.split("."))
+    return len(sentence.split(". "))
+  
+
+    
+        
